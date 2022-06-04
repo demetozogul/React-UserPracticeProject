4c467831fe72955bee882fe929b4f62d475a90d0
Demet Özoğul <98783120+demetozogul@users.noreply.github.com>
2022-06-04 19:38:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+
+function App() {
+
+  const [usersList, setUsersList] = useState([])
+
+  //state update function
+  const AddUserHandler = (uName, uAge) => {
+  
+    setUsersList((prevUsersList) => {
+      
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
+    
+  console.log("usersList",usersList)
+  return (
+    <div>
+      <AddUser onAddUser={AddUserHandler} />
+      <UsersList users={ usersList} />
+    </div>
+  );
+}
+
+export default App;
